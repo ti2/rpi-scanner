17469a5b51ab117e893b0af4e887b597e7d559ef
Chris Howard <cjhoward@cicadasoft.com>
2013-03-04 02:00:22 -0800

    Update license boilerplate

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2013  Chris Howard
  *
- * This file is part of rpi-scanner.
+ * This file is part of RPi Scanner.
  *
- * rpi-scanner is free software: you can redistribute it and/or modify
+ * RPi Scanner is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * rpi-scanner is distributed in the hope that it will be useful,
+ * RPi Scanner is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with rpi-scanner.  If not, see <http://www.gnu.org/licenses/>.
+ * along with RPi Scanner.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
